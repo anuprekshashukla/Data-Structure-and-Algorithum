df030c26ba6ca909e56f0de97e49d8e375b81f67
anuprekshashukla <83533944+anuprekshashukla@users.noreply.github.com>
2021-09-04 20:44:37 +0530

    Create subject.cpp

---
+++ subject.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+char *sub_name[7]={"1) Discrete Structure","2) Data Communication","3) Data Structure & Algorithum","4) Cloud Computing","5) Java Programming","6) Unix and Shell Programming","7) WEB Development Lab"};
+cout<<"All IIIrd SEMESTER SUBJECT:"<<endl<<endl;
+for(int i=0;i<7;i++)
+{
+  cout<<sub_name[i]<<endl;
+}
+getch();
+}
