78c37756d05371b665ad3cfdaf2b3d1ebc1ad55a
anuprekshashukla <83533944+anuprekshashukla@users.noreply.github.com>
2021-09-04 20:33:30 +0530

    Create Reverse.cpp

---
+++ Reverse.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void  main()
+{
+int i;
+clrscr();
+char arr[10]={'A','H','S','K','E','R','P','U','N','A'};
+cout<<"REVERSE THE ARRAY ELEMENT:"<<endl;
+for(i=9;i>=0;i--)
+{
+ cout<<arr[i];
+}
+getch();
+}
