f146251251e7eb6b7ebae0297b46b963affccd89
anuprekshashukla <83533944+anuprekshashukla@users.noreply.github.com>
2021-09-07 09:41:16 +0530

    Create Struct_Building.cpp

---
+++ Struct_Building.cpp
@@ -0,0 +1,36 @@
+#include<iostream.h>
+#include<conio.h>
+struct Building
+{
+int No_of_Gates;
+char name[20];
+int floor;
+int No_Of_Security_Gards;
+int No_of_lifts;
+int parking_area;
+int No_of_Blocks;
+};
+int main()
+ {
+  clrscr();
+  struct Building build[3];
+  for(int i=0;i<3;i++)
+   {
+     cout<<"\t\tBUILDING DETAILS:"<<i+1<<endl<<endl;
+     cout<<"Enter the Name of Building:"<<endl;
+     cin>>build[i].name;
+     cout<<"Enter the No. of Floor:"<<endl;
+     cin>>build[i].floor;
+     cout<<"Enter the No.of Security Gards:"<<endl;
+     cin>>build[i].No_Of_Security_Gards;
+     cout<<"Enter the NO. of Lifts:"<<endl;
+     cin>>build[i].No_of_lifts;
+     cout<<"Enter Parking Area:"<<endl;
+     cin>>build[i].parking_area;
+     cout<<"Enter No of Block:"<<endl;
+     cin>>build[i].No_of_Blocks;
+     cout<<"Enter No of Gates:"<<endl;
+     cin>>build[i].No_of_Gates;
+   }
+   return 0;
+ }
