d71be2a2b8a34f7b8a0e4d1ef7e64ac15418b3df
anuprekshashukla <83533944+anuprekshashukla@users.noreply.github.com>
2021-12-03 13:48:04 +0530

    Create Queue.cpp

---
+++ Queue.cpp
@@ -0,0 +1,103 @@
+//Implementation of Queue using Linked List
+
+#include<iostream.h>
+#include<conio.h>
+ 
+struct Node{
+    int data;
+    Node *next;
+};
+ 
+class Queue{
+   public:
+    Node *front,*rear;
+    Queue(){front=rear=NULL;}
+ 
+    void insert(int n);
+    void deleteitem();
+    void display();
+    ~Queue();
+};
+ 
+void Queue::insert(int n){
+    Node *temp=new Node;
+    if(temp==NULL){
+        cout<<"Anupreksha it is Overflow"<<endl;
+        return;
+    }
+    temp->data=n;
+    temp->next=NULL;
+ 
+    //for first node
+    if(front==NULL){
+        front=rear=temp;
+    }
+    else{
+        rear->next=temp;
+        rear=temp;
+    }
+
+    cout<<n<<" has been inserted successfully."<<endl;
+}
+ 
+void Queue::display(){
+    if(front==NULL){
+        cout<<"Anupreksha it is Underflow."<<endl;
+        return;
+    }
+    Node *temp=front;
+    //will check until NULL is not found
+    while(temp){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+ 
+void Queue :: deleteitem()
+    {
+    if (front==NULL){
+        cout<<"Anupreksha it is  underflow"<<endl;
+        return;
+    }
+     
+    cout<<front->data<<" is being deleted "<<endl;
+    if(front==rear)//if only one node is there
+        front=rear=NULL;
+    else
+        front=front->next;
+}
+ 
+Queue ::~Queue()
+{
+    while(front!=NULL)
+    {
+        Node *temp=front;
+        front=front->next;
+        delete temp;
+    }
+    rear=NULL;
+}
+ 
+ 
+int main(){
+clrscr();
+    Queue Q;
+    Q.display();
+ 
+    Q.insert(10);
+    Q.insert(24);
+    Q.insert(28);
+    Q.insert(32);
+    Q.insert(30);
+     
+    Q.display();
+     
+    Q.deleteitem();
+    Q.deleteitem();
+    Q.deleteitem();
+    Q.deleteitem();
+    Q.deleteitem();
+     
+    getch();
+}
